882f7ea395ea68d44172d9e62b20ff57dfef7585
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-24 22:03:18 +0200

    Add PinView

---
+++ src/NodeEditor/Views/PinView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NodeEditor.Views
+{
+    public class PinView : UserControl
+    {
+        public PinView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
