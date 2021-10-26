0f1177a228238af770659ceb6463b2939a8ecc4f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-26 22:29:56 +0200

    Add serializer

---
+++ samples/NodeEditorDemo/NodeSerializer.cs
@@ -0,0 +1,49 @@
+using System.Text;
+using Newtonsoft.Json;
+
+namespace NodeEditorDemo
+{
+    public static class NodeSerializer
+    {
+        private static readonly JsonSerializerSettings s_settings;
+
+        static NodeSerializer()
+        {
+            s_settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+        }
+
+        public static string Serialize<T>(T value)
+        {
+            return JsonConvert.SerializeObject(value, s_settings);
+        }
+
+        public static T Deserialize<T>(string text)
+        {
+            return JsonConvert.DeserializeObject<T>(text, s_settings)!;
+        }
+
+        public static T Load<T>(string path)
+        {
+            using var stream = System.IO.File.OpenRead(path);
+            using var streamReader = new System.IO.StreamReader(stream, Encoding.UTF8);
+            var text = streamReader.ReadToEnd();
+            return Deserialize<T>(text);
+        }
+
+        public static void Save<T>(string path, T value)
+        {
+            var text = Serialize<T>(value);
+            if (string.IsNullOrWhiteSpace(text)) return;
+            using var stream = System.IO.File.Create(path);
+            using var streamWriter = new System.IO.StreamWriter(stream, Encoding.UTF8);
+            streamWriter.Write(text);
+        }
+    }
+}
--- samples/NodeEditorDemo/ViewModels/MainWindowViewModel.cs
+++ samples/NodeEditorDemo/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,14 @@
-﻿using NodeEditor.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Shapes;
+using NodeEditor.ViewModels;
 using ReactiveUI;
 
 namespace NodeEditorDemo.ViewModels
@@ -7,10 +17,67 @@ namespace NodeEditorDemo.ViewModels
     {
         private DrawingNodeViewModel? _drawing;
 
+        public MainWindowViewModel()
+        {
+            OpenCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var dlg = new OpenFileDialog { AllowMultiple = false };
+                dlg.Filters.Add(new FileDialogFilter() { Name = "Json Files (*.json)", Extensions = new List<string> { "json" } });
+                dlg.Filters.Add(new FileDialogFilter() { Name = "All Files (*.*)", Extensions = new List<string> { "*" } });
+                var result = await dlg.ShowAsync((Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow);
+                if (result is { } && result.Length == 1)
+                {
+                    try
+                    {
+                        var json = await Task.Run(() =>  System.IO.File.ReadAllText(result.First()));
+                        var drawing = NodeSerializer.Deserialize<DrawingNodeViewModel?>(json);
+                        if (drawing is { })
+                        {
+                            Drawing = drawing;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                        Debug.WriteLine(ex.StackTrace);
+                    }
+                }
+            });
+
+            SaveCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var dlg = new SaveFileDialog();
+                dlg.Filters.Add(new FileDialogFilter() { Name = "Json Files (*.json)", Extensions = new List<string> { "json" } });
+                dlg.Filters.Add(new FileDialogFilter() { Name = "All Files (*.*)", Extensions = new List<string> { "*" } });
+                dlg.InitialFileName = System.IO.Path.GetFileNameWithoutExtension("drawing");
+                var result = await dlg.ShowAsync((Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow);
+                if (result is { })
+                {
+                    try
+                    {
+                        await Task.Run(() =>
+                        {
+                            var json = NodeSerializer.Serialize(_drawing);
+                            System.IO.File.WriteAllText(result, json);
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                        Debug.WriteLine(ex.StackTrace);
+                    }
+                }
+            });
+        }
+
         public DrawingNodeViewModel? Drawing
         {
             get => _drawing;
             set => this.RaiseAndSetIfChanged(ref _drawing, value);
         }
+
+        public ICommand OpenCommand { get; }
+
+        public ICommand SaveCommand { get; }
     }
 }
