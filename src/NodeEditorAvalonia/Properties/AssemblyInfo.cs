d3dca70bab11621af1a40384a6a15d7d167483b7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-27 12:23:49 +0200

    Create AssemblyInfo.cs

---
+++ src/NodeEditorAvalonia/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using Avalonia.Metadata;
+
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "NodeEditor.Controls")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "NodeEditor.Converters")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "NodeEditor.ViewModels")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "NodeEditor.Views")]
