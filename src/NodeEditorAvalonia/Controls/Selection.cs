745070e093a607e4904456fe8b151787c45f077e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-03 23:21:45 +0100

    Create Selection.cs

---
+++ src/NodeEditorAvalonia/Controls/Selection.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace NodeEditor.Controls
+{
+    public class Selection : Control
+    {
+        public static readonly StyledProperty<IBrush?> BrushProperty =
+            AvaloniaProperty.Register<Selection, IBrush?>(nameof(Brush), new SolidColorBrush(Colors.Blue) { Opacity = 0.5 });
+
+        public static readonly StyledProperty<IPen?> PenProperty =
+            AvaloniaProperty.Register<Selection, IPen?>(nameof(Pen), new Pen(new SolidColorBrush(Colors.Blue), 2));
+
+        public static readonly StyledProperty<Point> TopLeftProperty =
+            AvaloniaProperty.Register<Selection, Point>(nameof(TopLeft));
+
+        public static readonly StyledProperty<Point> BottomRightProperty =
+            AvaloniaProperty.Register<Selection, Point>(nameof(BottomRight));
+
+        public IBrush? Brush
+        {
+            get => GetValue(BrushProperty);
+            set => SetValue(BrushProperty, value);
+        }
+
+        public IPen? Pen
+        {
+            get => GetValue(PenProperty);
+            set => SetValue(PenProperty, value);
+        }
+
+        public Point TopLeft
+        {
+            get => GetValue(TopLeftProperty);
+            set => SetValue(TopLeftProperty, value);
+        }
+
+        public Point BottomRight
+        {
+            get => GetValue(BottomRightProperty);
+            set => SetValue(BottomRightProperty, value);
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            var adornedElement = GetValue(AdornerLayer.AdornedElementProperty);
+            if (adornedElement is null)
+            {
+                return;
+            }
+ 
+            context.DrawRectangle(Brush, Pen, new Rect(TopLeft, BottomRight));
+        }
+    }
+}
