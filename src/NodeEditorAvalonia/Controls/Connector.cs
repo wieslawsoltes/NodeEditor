4a2b23f93d9c3aa7cd023268627a7e5a279d9aaf
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-07 18:50:21 +0100

    Update Connector.cs

--- src/NodeEditorAvalonia/Controls/Connector.cs
+++ src/NodeEditorAvalonia/Controls/Connector.cs
@@ -48,18 +48,18 @@ namespace NodeEditor.Controls
 
             context.BeginFigure(StartPoint, false);
 
-            if (DataContext is IConnector connectorViewModel)
+            if (DataContext is IConnector connector)
             {
                 var p1X = StartPoint.X;
                 var p1Y = StartPoint.Y;
                 var p2X = EndPoint.X;
                 var p2Y = EndPoint.Y;
 
-                connectorViewModel.GetControlPoints(
-                    connectorViewModel.Orientation, 
+                connector.GetControlPoints(
+                    connector.Orientation, 
                     Offset, 
-                    connectorViewModel.Start?.Alignment ?? PinAlignment.None,
-                    connectorViewModel.End?.Alignment ?? PinAlignment.None,
+                    connector.Start?.Alignment ?? PinAlignment.None,
+                    connector.End?.Alignment ?? PinAlignment.None,
                     ref p1X, ref p1Y, 
                     ref p2X, ref p2Y);
 
