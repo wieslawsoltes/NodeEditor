770a9208e3b1694e407b893cd4bcf16031a5a78c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-13 23:56:02 +0100

    Update Program.cs

--- samples/NodeEditorDemo/Program.cs
+++ samples/NodeEditorDemo/Program.cs
@@ -13,6 +13,18 @@ namespace NodeEditorDemo
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .With(new Win32PlatformOptions()
+                {
+                    UseDeferredRendering = true
+                })
+                .With(new X11PlatformOptions()
+                {
+                    UseDeferredRendering = true
+                })
+                .With(new AvaloniaNativePlatformOptions()
+                {
+                    UseDeferredRendering = true
+                })
                 .LogToTrace()
                 .UseReactiveUI()
                 .UseSkia();
