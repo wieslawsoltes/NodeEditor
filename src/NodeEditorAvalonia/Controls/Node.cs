a2b155ba22bff17375db703c66af506414bb1f4f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-20 23:24:16 +0100

    Add :selected pseudo classes

--- src/NodeEditorAvalonia/Controls/Node.cs
+++ src/NodeEditorAvalonia/Controls/Node.cs
@@ -1,7 +1,9 @@
-﻿using Avalonia.Controls.Primitives;
+﻿using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
 
 namespace NodeEditor.Controls
 {
+    [PseudoClasses(":selected")]
     public class Node : TemplatedControl
     {
     }
