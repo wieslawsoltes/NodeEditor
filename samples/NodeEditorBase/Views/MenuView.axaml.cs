0853e50c1c70f3ac8bc88130dd647584d892a71f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-24 12:15:11 +0100

    Refactor main view

---
+++ samples/NodeEditorBase/Views/MenuView.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.PanAndZoom;
+using Avalonia.Markup.Xaml;
+using NodeEditorDemo.Controls;
+
+namespace NodeEditorDemo.Views;
+
+public class MenuView : UserControl
+{
+    public static readonly StyledProperty<NodeZoomBorder?> ZoomControlProperty = 
+        AvaloniaProperty.Register<MenuView, NodeZoomBorder?>(nameof(ZoomControl));
+
+    public MenuView()
+    {
+        InitializeComponent();
+    }
+
+    public NodeZoomBorder? ZoomControl
+    {
+        get => GetValue(ZoomControlProperty);
+        set => SetValue(ZoomControlProperty, value);
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
