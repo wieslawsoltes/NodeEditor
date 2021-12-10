495edcdc7b2ba1f522f68e01dcc7053a96d1ac11
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-12-10 22:16:24 +0100

    Split into separate views

---
+++ samples/NodeEditor.Base/Views/DrawingView.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using NodeEditorDemo.Controls;
+
+namespace NodeEditorDemo.Views;
+
+public class DrawingView : UserControl
+{
+    public static readonly StyledProperty<NodeZoomBorder?> ZoomControlProperty = 
+        AvaloniaProperty.Register<DrawingView, NodeZoomBorder?>(nameof(ZoomControl));
+
+    public DrawingView()
+    {
+        InitializeComponent();
+    }
+
+    public NodeZoomBorder? ZoomControl
+    {
+        get => GetValue(ZoomControlProperty);
+        set => SetValue(ZoomControlProperty, value);
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ samples/NodeEditor.Base/Views/ToolboxView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NodeEditorDemo.Views;
+
+public class ToolboxView : UserControl
+{
+    public ToolboxView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
