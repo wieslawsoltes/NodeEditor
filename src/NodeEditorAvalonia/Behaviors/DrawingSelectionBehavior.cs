7d858a56df4d9f523a13230375d25029f3a7e4d3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-12-13 11:16:12 +0100

    Update selected rectangle after moving nodes

--- src/NodeEditorAvalonia/Behaviors/DrawingSelectionBehavior.cs
+++ src/NodeEditorAvalonia/Behaviors/DrawingSelectionBehavior.cs
@@ -364,10 +364,6 @@ public class DrawingSelectionBehavior : Behavior<ItemsControl>
         var deltaY = position.Y - _start.Y;
         _start = position;
 
-        var selectedRect = HitTestHelper.CalculateSelectedRect(AssociatedObject);
-
-        _selectedRect = selectedRect;
-
         foreach (var node in drawingNode.SelectedNodes)
         {
             if (node.CanMove())
@@ -375,6 +371,10 @@ public class DrawingSelectionBehavior : Behavior<ItemsControl>
                 node.Move(deltaX, deltaY);
             }
         }
+
+        var selectedRect = HitTestHelper.CalculateSelectedRect(AssociatedObject);
+
+        _selectedRect = selectedRect;
 
         UpdateSelected(selectedRect);
     }
