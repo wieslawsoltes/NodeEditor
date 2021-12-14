17c2f2ee5a43e43d8815e214ee4a1a7d9fc0d2f4
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-12-14 14:50:54 +0100

    Add SettingsView

--- samples/NodeEditor.Base/Views/ToolboxView.axaml.cs
+++ samples/NodeEditor.Base/Views/ToolboxView.axaml.cs
@@ -1,34 +1,13 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using NodeEditor.Controls;
-using NodeEditorDemo.Controls;
 
 namespace NodeEditorDemo.Views;
 
 public class ToolboxView : UserControl
 {
-    public static readonly StyledProperty<NodeZoomBorder?> ZoomControlProperty = 
-        AvaloniaProperty.Register<ToolboxView, NodeZoomBorder?>(nameof(ZoomControl));
-
-    public static readonly StyledProperty<DrawingNode?> DrawingNodeProperty = 
-        AvaloniaProperty.Register<ToolboxView, DrawingNode?>(nameof(DrawingNode));
-
     public ToolboxView()
     {
         InitializeComponent();
-    }
-
-    public NodeZoomBorder? ZoomControl
-    {
-        get => GetValue(ZoomControlProperty);
-        set => SetValue(ZoomControlProperty, value);
-    }
-
-    public DrawingNode? DrawingNode
-    {
-        get => GetValue(DrawingNodeProperty);
-        set => SetValue(DrawingNodeProperty, value);
     }
 
     private void InitializeComponent()
