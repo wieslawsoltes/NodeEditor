17d4dde4c58cf4d923be93dc1a93579d4c1da3a7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-12-14 11:37:58 +0100

    Fix

--- src/NodeEditorAvalonia/Behaviors/DrawingPressedBehavior.cs
+++ src/NodeEditorAvalonia/Behaviors/DrawingPressedBehavior.cs
@@ -39,12 +39,10 @@ public class DrawingPressedBehavior : Behavior<ItemsControl>
 
         if (e.GetCurrentPoint(AssociatedObject).Properties.IsLeftButtonPressed)
         {
-            e.Pointer.Capture(AssociatedObject);
             drawingNode.DrawingLeftPressed(x, y);
         }
         else if (e.GetCurrentPoint(AssociatedObject).Properties.IsRightButtonPressed)
         {
-            e.Pointer.Capture(AssociatedObject);
             drawingNode.DrawingRightPressed(x, y);
         }
     }
