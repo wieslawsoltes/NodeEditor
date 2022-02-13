1fca29c282b59fb0ce5bcd37dcf213a2e3596e6e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-02-13 22:53:25 +0100

    Add android sample

---
+++ samples/NodeEditor.Android/MainActivity.cs
@@ -0,0 +1,47 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Avalonia;
+using Avalonia.Android;
+using NodeEditorDemo;
+using NodeEditorDemo.ViewModels;
+using NodeEditorDemo.Views;
+
+namespace NodeEditor.Android
+{
+    [Activity(Label = "Main",
+        MainLauncher = true,
+        Icon = "@drawable/icon",
+        LaunchMode = LaunchMode.SingleInstance/*,
+        ScreenOrientation = ScreenOrientation.Landscape*/)]
+    public class MainBaseActivity : AvaloniaActivity
+    {
+        static MainBaseActivity()
+        {
+            App.EnableInputOutput = true;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            if (Avalonia.Application.Current == null)
+            {
+                AppBuilder.Configure<App>()
+                    .UseAndroid()
+                    .SetupWithoutStarting();
+            }
+
+            base.OnCreate(savedInstanceState);
+
+            var mainView = new MainView
+            {
+                DataContext = new MainWindowViewModel
+                {
+                    IsEditMode = true,
+                    IsToolboxVisible = false
+                }
+            };
+
+            Content = mainView;
+        }
+    }
+}
