f2c1b2a4c58a08af44204c979d379d469a6b99ef
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-02-19 23:43:24 +0100

    Update App.axaml.cs

--- samples/NodeEditor.Base/App.axaml.cs
+++ samples/NodeEditor.Base/App.axaml.cs
@@ -15,30 +15,42 @@ public class App : Application
         AvaloniaXamlLoader.Load(this);
     }
 
+    public App()
+    {
+        Name = "NodeEditor";
+    }
+    
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            var vm = new MainWindowViewModel
+            {
+                IsEditMode = true,
+                IsToolboxVisible = true
+            };
+
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel
-                {
-                    IsEditMode = true,
-                    IsToolboxVisible = true
-                }
+                DataContext = vm
             };
+
+            DataContext = vm;
         }
             
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewLifetime)
         {
+            var vm = new MainWindowViewModel
+            {
+                IsEditMode = true,
+                IsToolboxVisible = false
+            };
             singleViewLifetime.MainView = new MainView
             {
-                DataContext = new MainWindowViewModel
-                {
-                    IsEditMode = true,
-                    IsToolboxVisible = false
-                }
+                DataContext = vm
             };
+
+            DataContext = vm;
         }
 
         base.OnFrameworkInitializationCompleted();
