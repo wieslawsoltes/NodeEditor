d60d4556077f78ae0b14f329e790ce35ca76f26c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-02-19 23:42:56 +0100

    Update MainView.axaml.cs

--- samples/NodeEditor.Base/Views/MainView.axaml.cs
+++ samples/NodeEditor.Base/Views/MainView.axaml.cs
@@ -1,13 +1,24 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using NodeEditorDemo.Controls;
 
 namespace NodeEditorDemo.Views;
 
 public class MainView : UserControl
 {
+    public static readonly StyledProperty<NodeZoomBorder?> ZoomControlProperty = 
+        AvaloniaProperty.Register<MenuView, NodeZoomBorder?>(nameof(MainView));
+
     public MainView()
     {
         InitializeComponent();
+    }
+
+    public NodeZoomBorder? ZoomControl
+    {
+        get => GetValue(ZoomControlProperty);
+        set => SetValue(ZoomControlProperty, value);
     }
 
     private void InitializeComponent()
