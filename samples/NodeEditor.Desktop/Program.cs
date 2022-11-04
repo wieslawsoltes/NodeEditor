3670c36648b8d784ab7e88b58e53d80c638d4e80
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-05 00:18:43 +0100

    Enable NativeAOT

--- samples/NodeEditor.Desktop/Program.cs
+++ samples/NodeEditor.Desktop/Program.cs
@@ -7,8 +7,18 @@ namespace NodeEditorDemo;
 class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 
     static Program()
     {
