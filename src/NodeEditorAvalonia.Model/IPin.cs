2d1e9067fff64766464f7285c679a76acf962cf1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-09 21:05:58 +0100

    Update IPin.cs

--- src/NodeEditorAvalonia.Model/IPin.cs
+++ src/NodeEditorAvalonia.Model/IPin.cs
@@ -2,6 +2,104 @@ using System;
 
 namespace NodeEditor.Model;
 
+public sealed class PinCreatedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public PinCreatedEventArgs(IPin? pin)
+    {
+        Pin = pin;
+    }
+}
+
+public sealed class PinRemovedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public PinRemovedEventArgs(IPin? pin)
+    {
+        Pin = pin;
+    }
+}
+
+public sealed class PinMovedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public double X { get; }
+
+    public double Y { get; }
+
+    public PinMovedEventArgs(IPin? pin, double x, double y)
+    {
+        X = x;
+        Y = y;
+        Pin = pin;
+    }
+}
+
+public sealed class PinSelectedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public PinSelectedEventArgs(IPin? pin)
+    {
+        Pin = pin;
+    }
+}
+
+public class PinDeselectedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public PinDeselectedEventArgs(IPin? pin)
+    {
+        Pin = pin;
+    }
+}
+
+public sealed class PinResizedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public double X { get; }
+
+    public double Y { get; }
+
+    public double Width { get; }
+
+    public double Height { get; }
+
+    public PinResizedEventArgs(IPin? pin, double x, double y, double width, double height)
+    {
+        Pin = pin;
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+}
+
+public sealed class PinConnectedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public PinConnectedEventArgs(IPin? pin)
+    {
+        Pin = pin;
+    }
+}
+
+public sealed class PinDisconnectedEventArgs : EventArgs
+{
+    public IPin? Pin { get; }
+
+    public PinDisconnectedEventArgs(IPin? pin)
+    {
+        Pin = pin;
+    }
+}
+
 public interface IPin
 {
     string? Name { get; set; }
