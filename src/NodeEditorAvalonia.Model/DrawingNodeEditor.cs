84ce53384508f4919c6a7568306d89aa8335fedd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-09 21:42:15 +0100

    Update DrawingNodeEditor.cs

--- src/NodeEditorAvalonia.Model/DrawingNodeEditor.cs
+++ src/NodeEditorAvalonia.Model/DrawingNodeEditor.cs
@@ -245,14 +245,7 @@ public sealed class DrawingNodeEditor
                 {
                     _node.Nodes?.Remove(node);
                     node.OnRemoved();
-
-                    if (node.Pins is { })
-                    {
-                        foreach (var pin in node.Pins)
-                        {
-                            pin.OnRemoved();
-                        }
-                    }
+                    NotifyPinsRemoved(node);
                 }
             }
         }
