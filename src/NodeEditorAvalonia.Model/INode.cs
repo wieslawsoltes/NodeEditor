5b0e1edbaedb7ee0801f6b1c9b835da009cea1b3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-09 21:05:53 +0100

    Update INode.cs

--- src/NodeEditorAvalonia.Model/INode.cs
+++ src/NodeEditorAvalonia.Model/INode.cs
@@ -3,6 +3,84 @@ using System.Collections.Generic;
 
 namespace NodeEditor.Model;
 
+public sealed class NodeCreatedEventArgs : EventArgs
+{
+    public INode? Node { get; }
+
+    public NodeCreatedEventArgs(INode? node)
+    {
+        Node = node;
+    }
+}
+
+public sealed class NodeRemovedEventArgs : EventArgs
+{
+    public INode? Node { get; }
+
+    public NodeRemovedEventArgs(INode? node)
+    {
+        Node = node;
+    }
+}
+
+public sealed class NodeMovedEventArgs : EventArgs
+{
+    public INode? Node { get; }
+
+    public double X { get; }
+
+    public double Y { get; }
+
+    public NodeMovedEventArgs(INode? node, double x, double y)
+    {
+        X = x;
+        Y = y;
+        Node = node;
+    }
+}
+
+public sealed class NodeSelectedEventArgs : EventArgs
+{
+    public INode? Node { get; }
+
+    public NodeSelectedEventArgs(INode? node)
+    {
+        Node = node;
+    }
+}
+
+public sealed class NodeDeselectedEventArgs : EventArgs
+{
+    public INode? Node { get; }
+
+    public NodeDeselectedEventArgs(INode? node)
+    {
+        Node = node;
+    }
+}
+
+public sealed class NodeResizedEventArgs : EventArgs
+{
+    public INode? Node { get; }
+
+    public double X { get; }
+
+    public double Y { get; }
+
+    public double Width { get; }
+
+    public double Height { get; }
+
+    public NodeResizedEventArgs(INode? node, double x, double y, double width, double height)
+    {
+        Node = node;
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+}
+
 public interface INode
 {
     string? Name { get; set; }
