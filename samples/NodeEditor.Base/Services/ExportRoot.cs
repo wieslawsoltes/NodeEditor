ed62250cebd5c8ff53e99ab703fb6d0b245aa288
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-19 07:58:47 +0100

    Update ExportRoot.cs

--- samples/NodeEditor.Base/Services/ExportRoot.cs
+++ samples/NodeEditor.Base/Services/ExportRoot.cs
@@ -10,7 +10,7 @@ using Avalonia.Styling;
 
 namespace NodeEditorDemo.Services;
 
-public class ExportRoot : Decorator, IFocusScope, ILayoutRoot, IInputRoot, IRenderRoot, IStyleHost, ILogicalRoot
+internal class ExportRoot : Decorator, IFocusScope, ILayoutRoot, IInputRoot, IRenderRoot, IStyleHost, ILogicalRoot
 {
     public ExportRoot()
     {
