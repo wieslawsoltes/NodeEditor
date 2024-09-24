8731f29587049202f32ea48517bd0446e82e019e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-09-24 23:03:06 +0200

    Refactor Toolbox

--- src/NodeEditorAvalonia/Controls/Toolbox.cs
+++ src/NodeEditorAvalonia/Controls/Toolbox.cs
@@ -1,7 +1,27 @@
+using System.Collections;
+using Avalonia;
 using Avalonia.Controls.Primitives;
+using NodeEditor.Model;
 
 namespace NodeEditor.Controls;
 
 public class Toolbox : TemplatedControl
 {
+    public static readonly StyledProperty<IEnumerable?> TemplatesSourceProperty =
+        AvaloniaProperty.Register<Toolbox, IEnumerable?>(nameof(TemplatesSource));
+    
+    public static readonly StyledProperty<IDrawingNode?> DrawingProperty = 
+        AvaloniaProperty.Register<Toolbox, IDrawingNode?>(nameof(Drawing));
+
+    public IEnumerable? TemplatesSource
+    {
+        get => GetValue(TemplatesSourceProperty);
+        set => SetValue(TemplatesSourceProperty, value);
+    }
+    
+    public IDrawingNode? Drawing
+    {
+        get => GetValue(DrawingProperty);
+        set => SetValue(DrawingProperty, value);
+    }
 }
