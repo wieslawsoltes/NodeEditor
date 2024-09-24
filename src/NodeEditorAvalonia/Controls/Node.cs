b3fffab6ac0707f47416732deab5638a2ab56375
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-09-24 10:34:51 +0200

    Refactor Node and Pins templates

--- src/NodeEditorAvalonia/Controls/Node.cs
+++ src/NodeEditorAvalonia/Controls/Node.cs
@@ -1,9 +1,19 @@
-﻿using Avalonia.Controls.Metadata;
-using Avalonia.Controls.Primitives;
+﻿using System.Collections;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
 
 namespace NodeEditor.Controls;
 
 [PseudoClasses(":selected")]
-public class Node : TemplatedControl
+public class Node : ContentControl
 {
+    public static readonly StyledProperty<IEnumerable?> PinsSourceProperty =
+        AvaloniaProperty.Register<Node, IEnumerable?>(nameof(PinsSource));
+    
+    public IEnumerable? PinsSource
+    {
+        get => GetValue(PinsSourceProperty);
+        set => SetValue(PinsSourceProperty, value);
+    }
 }
--- src/NodeEditorAvalonia/Controls/Pins.cs
+++ src/NodeEditorAvalonia/Controls/Pins.cs
@@ -1,7 +1,17 @@
+using System.Collections;
+using Avalonia;
 using Avalonia.Controls.Primitives;
 
 namespace NodeEditor.Controls;
 
 public class Pins : TemplatedControl
 {
+    public static readonly StyledProperty<IEnumerable?> PinsSourceProperty =
+        AvaloniaProperty.Register<Pins, IEnumerable?>(nameof(PinsSource));
+    
+    public IEnumerable? PinsSource
+    {
+        get => GetValue(PinsSourceProperty);
+        set => SetValue(PinsSourceProperty, value);
+    }
 }
