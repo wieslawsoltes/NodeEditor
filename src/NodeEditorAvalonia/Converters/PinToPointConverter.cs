00ab2082a63b2110c3e4363a1cb0be96ec5d9556
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-09-24 22:45:22 +0200

    Update PinToPointConverter.cs

--- src/NodeEditorAvalonia/Converters/PinToPointConverter.cs
+++ src/NodeEditorAvalonia/Converters/PinToPointConverter.cs
@@ -12,21 +12,22 @@ public class PinToPointConverter : IValueConverter
 
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is IPin pin)
+        if (value is not IPin pin)
         {
-            var x = pin.X;
-            var y = pin.Y;
-
-            if (pin.Parent is not null)
-            {
-                x += pin.Parent.X;
-                y += pin.Parent.Y;
-            }
-
-            return new Point(x, y);
+            return new Point();
         }
 
-        return new Point();
+        var x = pin.X;
+        var y = pin.Y;
+
+        if (pin.Parent is not null)
+        {
+            x += pin.Parent.X;
+            y += pin.Parent.Y;
+        }
+
+        return new Point(x, y);
+
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
