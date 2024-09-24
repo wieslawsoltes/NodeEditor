8127178fa926a7100c27778522fb4eaa37431328
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-09-24 22:49:22 +0200

    Refactor Nodes

--- src/NodeEditorAvalonia/Controls/Nodes.cs
+++ src/NodeEditorAvalonia/Controls/Nodes.cs
@@ -1,7 +1,17 @@
+using System.Collections;
+using Avalonia;
 using Avalonia.Controls.Primitives;
 
 namespace NodeEditor.Controls;
 
 public class Nodes : TemplatedControl
 {
+    public static readonly StyledProperty<IEnumerable?> NodesSourceProperty =
+        AvaloniaProperty.Register<Nodes, IEnumerable?>(nameof(NodesSource));
+    
+    public IEnumerable? NodesSource
+    {
+        get => GetValue(NodesSourceProperty);
+        set => SetValue(NodesSourceProperty, value);
+    }
 }
