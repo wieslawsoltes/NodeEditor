31ddd7221eecf17d013e73f59ae9e01dafe566ff
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-09-24 21:58:07 +0200

    Refactor Connectors

--- src/NodeEditorAvalonia/Controls/Connectors.cs
+++ src/NodeEditorAvalonia/Controls/Connectors.cs
@@ -1,7 +1,17 @@
+using System.Collections;
+using Avalonia;
 using Avalonia.Controls.Primitives;
 
 namespace NodeEditor.Controls;
 
 public class Connectors : TemplatedControl
 {
+    public static readonly StyledProperty<IEnumerable?> ConnectorsSourceProperty =
+        AvaloniaProperty.Register<Connectors, IEnumerable?>(nameof(ConnectorsSource));
+    
+    public IEnumerable? ConnectorsSource
+    {
+        get => GetValue(ConnectorsSourceProperty);
+        set => SetValue(ConnectorsSourceProperty, value);
+    }
 }
