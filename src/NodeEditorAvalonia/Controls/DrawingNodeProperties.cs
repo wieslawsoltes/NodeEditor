d15749f7249f9f309dc4a91d4ce1247291df520c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-09-26 22:53:40 +0200

    Refactor Editor

--- src/NodeEditorAvalonia/Controls/DrawingNodeProperties.cs
+++ src/NodeEditorAvalonia/Controls/DrawingNodeProperties.cs
@@ -1,16 +1,80 @@
 using Avalonia;
 using Avalonia.Controls.Primitives;
+using Avalonia.Data;
 
 namespace NodeEditor.Controls;
 
 public class DrawingNodeProperties : TemplatedControl
 {
-    public static readonly StyledProperty<DrawingNode?> DrawingNodeProperty = 
-        AvaloniaProperty.Register<DrawingNodeProperties, DrawingNode?>(nameof(DrawingNode));
+    public static readonly StyledProperty<bool> EnableSnapProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, bool>(nameof(EnableSnap), false, false, BindingMode.TwoWay);
 
-    public DrawingNode? DrawingNode
+    public static readonly StyledProperty<double> SnapXProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, double>(nameof(SnapX), 1.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> SnapYProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, double>(nameof(SnapY), 1.0, false, BindingMode.TwoWay);
+    
+    public static readonly StyledProperty<bool> EnableGridProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, bool>(nameof(EnableGrid), false, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> GridCellWidthProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, double>(nameof(GridCellWidth), 15.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> GridCellHeightProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, double>(nameof(GridCellHeight), 15.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> DrawingWidthProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, double>(nameof(DrawingWidth), 0.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> DrawingHeightProperty = 
+        AvaloniaProperty.Register<DrawingNodeProperties, double>(nameof(DrawingHeight), 0.0, false, BindingMode.TwoWay);
+
+    public bool EnableSnap
     {
-        get => GetValue(DrawingNodeProperty);
-        set => SetValue(DrawingNodeProperty, value);
+        get => GetValue(EnableSnapProperty);
+        set => SetValue(EnableSnapProperty, value);
+    }
+
+    public double SnapX
+    {
+        get => GetValue(SnapXProperty);
+        set => SetValue(SnapXProperty, value);
+    }
+
+    public double SnapY
+    {
+        get => GetValue(SnapYProperty);
+        set => SetValue(SnapYProperty, value);
+    }
+
+    public bool EnableGrid
+    {
+        get => GetValue(EnableGridProperty);
+        set => SetValue(EnableGridProperty, value);
+    }
+
+    public double GridCellWidth
+    {
+        get => GetValue(GridCellWidthProperty);
+        set => SetValue(GridCellWidthProperty, value);
+    }
+
+    public double GridCellHeight
+    {
+        get => GetValue(GridCellHeightProperty);
+        set => SetValue(GridCellHeightProperty, value);
+    }
+
+    public double DrawingWidth
+    {
+        get => GetValue(DrawingWidthProperty);
+        set => SetValue(DrawingWidthProperty, value);
+    }
+
+    public double DrawingHeight
+    {
+        get => GetValue(DrawingHeightProperty);
+        set => SetValue(DrawingHeightProperty, value);
     }
 }
--- src/NodeEditorAvalonia/Controls/Editor.cs
+++ src/NodeEditorAvalonia/Controls/Editor.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
+using Avalonia.Data;
 
 namespace NodeEditor.Controls;
 
@@ -10,14 +12,119 @@ namespace NodeEditor.Controls;
 [TemplatePart("PART_AdornerCanvas", typeof(Canvas))]
 public class Editor : TemplatedControl
 {
+    public static readonly StyledProperty<IEnumerable?> NodesSourceProperty =
+        AvaloniaProperty.Register<Editor, IEnumerable?>(nameof(NodesSource));
+
+    public static readonly StyledProperty<IEnumerable?> ConnectorsSourceProperty =
+        AvaloniaProperty.Register<Editor, IEnumerable?>(nameof(ConnectorsSource));
+
+    public static readonly StyledProperty<Control?> InputSourceProperty = 
+        AvaloniaProperty.Register<Editor, Control?>(nameof(InputSource));
+
+    public static readonly StyledProperty<Canvas?> AdornerCanvasProperty = 
+        AvaloniaProperty.Register<Editor, Canvas?>(nameof(AdornerCanvas));
+
+    public static readonly StyledProperty<bool> EnableSnapProperty = 
+        AvaloniaProperty.Register<Editor, bool>(nameof(EnableSnap), false, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> SnapXProperty = 
+        AvaloniaProperty.Register<Editor, double>(nameof(SnapX), 1.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> SnapYProperty = 
+        AvaloniaProperty.Register<Editor, double>(nameof(SnapY), 1.0, false, BindingMode.TwoWay);
+    
+    public static readonly StyledProperty<bool> EnableGridProperty = 
+        AvaloniaProperty.Register<Editor, bool>(nameof(EnableGrid), false, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> GridCellWidthProperty = 
+        AvaloniaProperty.Register<Editor, double>(nameof(GridCellWidth), 15.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> GridCellHeightProperty = 
+        AvaloniaProperty.Register<Editor, double>(nameof(GridCellHeight), 15.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> DrawingWidthProperty = 
+        AvaloniaProperty.Register<Editor, double>(nameof(DrawingWidth), 0.0, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> DrawingHeightProperty = 
+        AvaloniaProperty.Register<Editor, double>(nameof(DrawingHeight), 0.0, false, BindingMode.TwoWay);
+
     public static readonly StyledProperty<NodeZoomBorder?> ZoomControlProperty = 
         AvaloniaProperty.Register<Editor, NodeZoomBorder?>(nameof(ZoomControl));
 
     public static readonly StyledProperty<DrawingNode?> DrawingNodeProperty = 
         AvaloniaProperty.Register<Editor, DrawingNode?>(nameof(DrawingNode));
 
-    public static readonly StyledProperty<Canvas?> AdornerCanvasProperty = 
-        AvaloniaProperty.Register<Editor, Canvas?>(nameof(AdornerCanvas));
+    public IEnumerable? NodesSource
+    {
+        get => GetValue(NodesSourceProperty);
+        set => SetValue(NodesSourceProperty, value);
+    }
+
+    public IEnumerable? ConnectorsSource
+    {
+        get => GetValue(ConnectorsSourceProperty);
+        set => SetValue(ConnectorsSourceProperty, value);
+    }
+
+    public Control? InputSource
+    {
+        get => GetValue(InputSourceProperty);
+        set => SetValue(InputSourceProperty, value);
+    }
+
+    public Canvas? AdornerCanvas
+    {
+        get => GetValue(AdornerCanvasProperty);
+        set => SetValue(AdornerCanvasProperty, value);
+    }
+
+    public bool EnableSnap
+    {
+        get => GetValue(EnableSnapProperty);
+        set => SetValue(EnableSnapProperty, value);
+    }
+
+    public double SnapX
+    {
+        get => GetValue(SnapXProperty);
+        set => SetValue(SnapXProperty, value);
+    }
+
+    public double SnapY
+    {
+        get => GetValue(SnapYProperty);
+        set => SetValue(SnapYProperty, value);
+    }
+
+    public bool EnableGrid
+    {
+        get => GetValue(EnableGridProperty);
+        set => SetValue(EnableGridProperty, value);
+    }
+
+    public double GridCellWidth
+    {
+        get => GetValue(GridCellWidthProperty);
+        set => SetValue(GridCellWidthProperty, value);
+    }
+
+    public double GridCellHeight
+    {
+        get => GetValue(GridCellHeightProperty);
+        set => SetValue(GridCellHeightProperty, value);
+    }
+
+    public double DrawingWidth
+    {
+        get => GetValue(DrawingWidthProperty);
+        set => SetValue(DrawingWidthProperty, value);
+    }
+
+    public double DrawingHeight
+    {
+        get => GetValue(DrawingHeightProperty);
+        set => SetValue(DrawingHeightProperty, value);
+    }
 
     public NodeZoomBorder? ZoomControl
     {
@@ -29,12 +136,6 @@ public class Editor : TemplatedControl
     {
         get => GetValue(DrawingNodeProperty);
         set => SetValue(DrawingNodeProperty, value);
-    }
-
-    public Canvas? AdornerCanvas
-    {
-        get => GetValue(AdornerCanvasProperty);
-        set => SetValue(AdornerCanvasProperty, value);
     }
 
     protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
